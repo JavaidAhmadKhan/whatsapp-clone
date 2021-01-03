0ad9a9d7eb69134e1adf10930e87f4a714971e1e
JavaidAhmadKhan <relaxjavaid@gmail.com>
2021-01-03 23:57:59 +0530

    display messages in chat room

--- data/ChatRooms.ts
+++ data/ChatRooms.ts
@@ -12,7 +12,7 @@ export default [{
   lastMessage: {
     id: 'm1',
     content: 'Well done this sprint, guys!',
-    createdAt: '2020-10-03T14:48:00.000Z',
+    createdAt: '2020-12-03T13:48:00.000Z',
   }
 }, {
   id: '2',
@@ -28,7 +28,7 @@ export default [{
   lastMessage: {
     id: 'm2',
     content: 'How are you doing?',
-    createdAt: '2020-10-02T15:40:00.000Z',
+    createdAt: '2020-12-29T15:40:00.000Z',
   }
 }, {
   id: '3',
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, FlatList, ImageBackground } from "react-native";
 import { useRoute } from "@react-navigation/native";
-
+import chatRoomData from "../data/Chats";
+import ChatMessage from "../components/ChatMessage";
+import BG from "../assets/images/BG.png";
 const ChatRoomScreen = () => {
   const route = useRoute();
-  // console.log(route.params);
   return (
-    <View>
-      <Text>Chat Room</Text>
-    </View>
+    <ImageBackground style={{ width: "100%", height: "100%" }} source={BG}>
+      <FlatList
+        data={chatRoomData.messages}
+        renderItem={({ item }) => <ChatMessage message={item} />}
+        inverted
+      />
+    </ImageBackground>
   );
 };
 
